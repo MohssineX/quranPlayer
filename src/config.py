cd7d3a6d17a2a3aab7e7d5868417794618fb1258
Mohssine <254711500+MohssineX@users.noreply.github.com>
2026-08-12 04:03:02 +0100

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,71 @@
+# mimi is a nice cat 
+
+# github : https://github.com/MohssineX
+
+# Copyright (C) 2026 Mohssine <https://github.com/MohssineX>
+
+appName = "quranPlayer"
+
+# cCOLOR variables
+
+COLOR_GREEN = "\033[32m"
+COLOR_YELLOW = "\033[33m"
+COLOR_RED = "\033[31m"
+COLOR_RESET = "\033[0m"
+
+timeoutCfg = 5
+
+chunkDownload = 65536
+
+reciters = f"""{COLOR_YELLOW}╭─────────────────────────────────────────────────────────────────╮{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [1] Abdul Basit Abdus Samad    │ [16] Ali Jaber                 {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [2] Mahmoud Khalil Al-Husary   │ [17] Bandar Baleela            {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [3] Mohamed Siddiq Al-Minshawi │ [18] Khalid Al-Jalil           {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [4] Mishary Rashid Alafasy     │ [19] Abdullah Basfar           {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [5] Maher Al-Muaiqly           │ [20] Salah Bukhatir            {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [6] Yasser Al-Dosari           │ [21] Abdul Mohsen Al-Qasim     {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [7] Saad Al-Ghamdi             │ [22] Abdullah Al-Juhany        {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [8] Saud Al-Shuraim            │ [23] Salah Al-Budair           {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [9] Ahmed Al-Ajmi              │ [24] Hani Al-Rifai             {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [10] Abdul Rahman Al-Sudais    │ [25] Mohamed Gibril            {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [11] Abu Bakr Al-Shatri        │ [26] Mahmoud Ali Al-Banna      {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [12] Muhammad Ayyoub           │ [27] Mustafa Ismail            {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [13] Nasser Al-Qatami          │ [28] Abdul Bari Al-Thubaity    {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [14] Ali Al-Hudhaifi           │ [29] Wadee Al-Yamani           {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}│{COLOR_GREEN} [15] Khalifa Al-Tunaiji        │ [30] Khalid Al-Qahtani         {COLOR_YELLOW}│{COLOR_RESET}
+{COLOR_YELLOW}╰─────────────────────────────────────────────────────────────────╯{COLOR_RESET}"""
+
+reciters_urls = {
+
+    "1" : "https://server7.mp3quran.net/basit/",
+    "2" : "https://server13.mp3quran.net/husr/",
+    "3" : "https://server10.mp3quran.net/minsh/",
+    "4" : "https://server8.mp3quran.net/afs/",
+    "5" : "https://server12.mp3quran.net/maher/",
+    "6" : "https://server11.mp3quran.net/yasser/",
+    "7" : "https://server7.mp3quran.net/s_gmd/",
+    "8" : "https://server7.mp3quran.net/shur/",
+    "9" : "https://server10.mp3quran.net/ajm/",
+    "10" : "https://server11.mp3quran.net/sds/",
+    "11" : "https://server11.mp3quran.net/shatri/",
+    "12" : "https://server8.mp3quran.net/ayyub/",
+    "13" : "https://server6.mp3quran.net/qtm/",
+    "14" : "https://server9.mp3quran.net/hthfi/",
+    "15" : "https://server12.mp3quran.net/tnjy/",
+    "16" : "https://server11.mp3quran.net/a_jbr/",
+    "17" : "https://server6.mp3quran.net/balilah/",
+    "18" : "https://server10.mp3quran.net/jleel/",
+    "19" : "https://server6.mp3quran.net/bsfr/",
+    "20" : "https://server8.mp3quran.net/bu_khtr/",
+    "21" : "https://server8.mp3quran.net/qasm/",
+    "22" : "https://server13.mp3quran.net/jhn/",
+    "23" : "https://server6.mp3quran.net/s_bud/",
+    "24" : "https://server8.mp3quran.net/hani/",
+    "25" : "https://server8.mp3quran.net/jbrl/",
+    "26" : "https://server8.mp3quran.net/bna/",
+    "27" : "https://server8.mp3quran.net/mustafa/",
+    "28" : "https://server6.mp3quran.net/thubti/",
+    "29" : "https://server6.mp3quran.net/wdee3/",
+    "30" : "https://server10.mp3quran.net/qht/",
+
+}
