192d3be605b46875ff441c9b9d65eb516473ae61
Mohssine <254711500+MohssineX@users.noreply.github.com>
2026-09-05 03:00:33 +0100

    Enhance run.sh with error messages and checks

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
+ 
 if ! command -v pip &> /dev/null; then
-    echo "pip is not installed. Please install pip first."
+    echo "❌ pip is not installed. Please install pip first."
     exit 1
 fi
-
+ 
 pip install -r requirements.txt
 # pip install -r requirements.txt --break-system-packages
-
+ 
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+ 
 cd src/
 python3 main.py
